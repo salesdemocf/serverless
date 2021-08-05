26521bdc7b32fbd43ec853849148de0276e7fa1d
Dustin Van Buskirk <dev@vanbuskirk.me>
2021-08-04 17:58:35 -0700

    Update hello.py

--- lib/plugins/create/templates/azure-python/src/handlers/hello.py
+++ lib/plugins/create/templates/azure-python/src/handlers/hello.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello {name}! Excited to meet you!")
+        return func.HttpResponse(f"Hello {name}! Have a Nice Day!")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
